b4bd317e1305b3241af5cf007de3d2a698fd57f1
Bisquit <denis281120025911@mail.ru>
2023-07-12 14:05:26 +0300

    update

---
+++ server/routes/index.js
@@ -0,0 +1,14 @@
+const Router = require("express")
+const router = new Router()
+const employeeRouter = require('./employeeRouter')
+const positionRouter = require('./positionRouter')
+const technologyRouter = require('./technologyRouter')
+const workspaceRouter = require('./worspaceRouter')
+
+router.use('/employee', employeeRouter)
+router.use('/position', positionRouter)
+router.use('technology',technologyRouter)
+router.use('workspace', workspaceRouter)
+
+
+module.exports = router
