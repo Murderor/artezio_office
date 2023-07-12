594b1dec62aabe6984855ae7d15db751fa209a72
Bisquit <denis281120025911@mail.ru>
2023-07-12 13:24:19 +0300

    update

--- server/index.js
+++ server/index.js
@@ -9,9 +9,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/',(req, res) => {
-    res.status(200).json({message:'Working!!'})
-})
+
 
 const start = async () =>{
     try {
